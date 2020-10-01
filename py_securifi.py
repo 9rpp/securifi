26d7d22e07f574e66ced5d6fa8c39ca376f0cec9
ronny <ronnypau@gmail.com>
2020-10-01 12:14:21 -0400

    move all device operations into py_securifi.py

--- py_securifi.py
+++ py_securifi.py
@@ -1,4 +1,6 @@
 """An API Client to interact with Securifi Almond"""
+import time
+import json
 import logging
 
 from websocket import create_connection
@@ -12,92 +14,39 @@ _LOGGER = logging.getLogger(__name__)
 #_LOGGER.addHandler(ch)
 
 class securifi_almond():
-
     DEFAULT_PORT = 7681
     DEFAULT_USER = "admin"
 
     def __init__(self, host, pwd, port=DEFAULT_PORT, user=DEFAULT_USER):
-        self._host = host
-        self._port = port
-        self._user = user
-        self._pwd = pwd
-        self._ws = None
         self._devlist = {}
+        self._switches = []
 
+        # Initiate API communication
+        self._api_comm = self.api_comm(host, pwd, port, user)
 
-    def __open_conn(self):
-        url = "ws://" + self._host + ":" + str(self._port) + "/" + self._user + "/" + self._pwd
-        _LOGGER.debug("__open_conn: " + url)
-        self._ws = create_connection(url)
-        result = self._ws.recv()
-        if not result:
-            _LOGGER.error("Could not connect to Securifi Almond websocket")
-            self._ws = None
-            return False
-        rsp = eval(result)
-        if rsp['CommandType'] != "DynamicAlmondModeUpdated":
-            _LOGGER.error("Unexpected server response from connection")
-            self._ws = None
-            return False
-        return True
-
-    
-    def __close_conn(self):
-        _LOGGER.debug("__close_conn")
-        if not self._ws:
-            _LOGGER.error("Invalid websocket, unable to close connection")
-            return False
-        self._ws.close()
-        return True
-
-
-    def __send_cmd(self, mii, cmd):
-        _LOGGER.debug("__send_cmd: " + mii + "::" + cmd)
-        if not mii or not cmd:
-            _LOGGER.error("Invalid command params")
-            return 
-        if not self._ws:
-            _LOGGER.error("Websocket not established")
-            return
-        req = '{"MobileInternalIndex":"' + mii + '","CommandType":"'+ cmd +'"}'
-        self._ws.send(req)
-        _LOGGER.debug("sent")
-        result = self._ws.recv()
-        _LOGGER.debug("received")
-        if not result:
-            _LOGGER.error("Invalid results")
-            return
-        rsp = eval(result)
-        if rsp['CommandType'] != cmd or rsp['MobileInternalIndex'] != mii:
-            _LOGGER.error("Request and response mismatches")
-        return rsp
-
+        # Get initial list of switches
+        sw_list = self.__get_switches()
+        for sw in sw_list:
+            self._switches.append(self.switch(self._api_comm, str(sw), sw_list[sw]['name'], sw_list[sw]['state']))
+        #return self._switches
 
     def __get_devlist(self):
         _LOGGER.debug("__get_devlist")
-        self.__open_conn()
-        rsp = self.__send_cmd("1234", "DeviceList")
+        self._api_comm.open_conn()
+        rsp = self._api_comm.send_cmd("1234", "DeviceList")
         if not rsp:
             _LOGGER.error("Unable to retrieve DeviceList")
             return False
+        self._api_comm.close_conn()
         self._devlist = rsp
-        self.__close_conn()
         return True
 
 
-    def get_all_devices(self):
-        return self._devlist
-    
-
     # Queries devicelist and extract a list of switches
-    def get_switches(self):
+    def __get_switches(self):
         _LOGGER.debug("get_switches")
-#        if not self._devlist:
-#            self.__get_devlist()
-# TODO: get switches from cache? 
         self.__get_devlist()
         rsp = self._devlist
-        _LOGGER.debug(str(rsp))
         
         # Parse through rsp and grab only type 1 and 50 switches
         # get Data:FriendlyDeviceType, Data:Name, DeviceValues:2:Value
@@ -105,17 +54,172 @@ class securifi_almond():
         for dev in rsp['Devices']:
             dev_type = rsp['Devices'][dev]['Data']['Type']
             if dev_type == "1" or dev_type == "50":
-                #print(rsp['Devices'][dev]['Data']['FriendlyDeviceType'])
                 name = rsp['Devices'][dev]['Data']['Name']
                 state = True if rsp['Devices'][dev]['DeviceValues']['1']['Value'].lower() == "true" else False
                 devices[dev] = {"name": name, "state": state}
 
         return devices
+
+    def get_devlist(self):
+        return self._devlist
+
+    def get_switches(self):
+        return self._switches
+
+    def refresh_switches(self):
+        devs = self.__get_switches()
+        for sw in self._switches:
+            id = sw.get_devid()
+            sw.set_name(devs[id]['name'])
+            sw.set_state(devs[id]['state'])
+
+    class api_comm:
+        DEFAULT_PORT = 7681
+        DEFAULT_USER = "admin"
+
+        def __init__(self, host, pwd, port=DEFAULT_PORT, user=DEFAULT_USER):
+            _LOGGER.debug("API __init__")
+            self._host = host
+            self._port = port
+            self._user = user
+            self._pwd = pwd
+            self._ws = None
+
+        def open_conn(self):
+            url = "ws://" + self._host + ":" + str(self._port) + "/" + self._user + "/" + self._pwd
+            _LOGGER.debug("open_conn: " + url)
+            self._ws = create_connection(url)
+            result = self._ws.recv()
+            if not result:
+                _LOGGER.error("Could not connect to Securifi Almond websocket")
+                self._ws = None
+                return False
+            rsp = eval(result)
+            if rsp['CommandType'] != "DynamicAlmondModeUpdated":
+                _LOGGER.error("Unexpected server response from connection")
+                self._ws = None
+                return False
+            return True
+
+
+        def close_conn(self):
+            _LOGGER.debug("close_conn")
+            if not self._ws:
+                _LOGGER.error("Invalid websocket, unable to close connection")
+                return False
+            self._ws.close()
+            return True
+
+
+        def send_cmd(self, mii, cmd, devid=None, idx=None, val=None):
+            _LOGGER.debug("send_cmd: " + mii + "::" + cmd)
+            if not mii or not cmd:
+                _LOGGER.error("Invalid command params")
+                return
+            if not self._ws:
+                _LOGGER.error("Websocket not established")
+                return
+            cmd_dict = {}
+            cmd_dict["MobileInternalIndex"] = mii
+            cmd_dict["CommandType"] = cmd
+            if devid:
+                cmd_dict["ID"] = str(devid)
+            if idx:
+                cmd_dict["Index"] = str(idx)
+            if val:
+                cmd_dict["Value"] = str(val).lower()
+            req = json.dumps(cmd_dict)
+            _LOGGER.debug("sending::" + req)
+            self._ws.send(req)
+            _LOGGER.debug("sent")
+            result = self._ws.recv()
+            _LOGGER.debug("received::" + result)
+            if not result:
+                _LOGGER.error("Invalid results")
+                return
+            rsp = eval(result)
+            if rsp['CommandType'] != cmd or rsp['MobileInternalIndex'] != mii:
+                _LOGGER.error("Request and response mismatches")
+            return rsp
+
+
+        def update_device(self, devid, idx, val=False):
+            _LOGGER.debug("update_device: " + devid + "::" + idx + "::" + str(val))
+            if not devid or not idx:
+                _LOGGER.error("Invalid command params")
+                return
+            if not self._ws:
+                _LOGGER.error("Websocket not established")
+                return
+            self.open_conn()
+            rsp = self.send_cmd("1234", "UpdateDeviceIndex", devid, idx, str(val))
+            self.close_conn()
+            return True if rsp['Success'].lower() == "true" else False
+
+
+    class switch:
+        def __init__(self, api_comm, devid, name, state):
+            self._api_comm = api_comm
+            self._devid = devid
+            self._name = name
+            self._state = state
+            self._idx = "1" # property index 1 is always the on/off for switches
+
+        def print_attrib(self):
+            _LOGGER.debug("devid::"+self._devid+", name::"+self._name+", state::"+str(self._state))
+
+        def get_devid(self):
+            return self._devid
+
+        def get_name(self):
+            return self._name
+
+        def get_state(self):
+            return self._state
+
+        def set_name(self, name):
+            _LOGGER.debug("Setting " + self._devid + " name to " + name)
+            self._name = name
+
+        def set_state(self, state):
+            _LOGGER.debug("Setting " + self._devid + " state to " + str(state))
+            self._state = state
+
+        def turn_on(self):
+            self._api_comm.update_device(self._devid, self._idx, True)
+
+        def turn_off(self):
+            self._api_comm.update_device(self._devid, self._idx, False)
     
 def main():
     almond = securifi_almond("192.168.1.101", "third62")
-    all_sw = almond.get_switches()
-    _LOGGER.debug(str(all_sw))
+    sw_objs = almond.get_switches()
+    _LOGGER.debug(str(sw_objs))
+    for sw in sw_objs:
+        sw.print_attrib()
+
+    _LOGGER.debug(">>>>>>  Turning off sw0")
+    sw_objs[0].turn_off()
+    for sw in sw_objs:
+        sw.print_attrib()
+
+    time.sleep(10)
+
+    _LOGGER.debug(">>>>>> Turning on sw0 w/ refresh")
+    sw_objs[0].turn_on()
+    time.sleep(2)
+    almond.refresh_switches()
+    for sw in sw_objs:
+        sw.print_attrib()
+
+    time.sleep(10)
+
+    _LOGGER.debug(">>>>>> Turning off sw0 w/ refresh")
+    sw_objs[0].turn_off()
+    time.sleep(2)
+    almond.refresh_switches()
+    for sw in sw_objs:
+        sw.print_attrib()
 
 if __name__ == "__main__":
     main()
--- switch.py
+++ switch.py
@@ -28,51 +28,47 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_PASSWORD): cv.string,
 })
 
-async def fetch_switch_data():
-    ws = create_connection("ws://192.168.1.101:7681/admin/third62")
-    result = ws.recv()
-    if not result:
-        _LOGGER.error("Could not connect to Securifi Almond websocket")
-#    rsp = eval(result)
-#    if rsp['CommandType'] != "DynamicAlmondModeUpdated":
-#        raise Exception("Unexpected server handshake")
-
-    mii = "1234"
-    cmd = "DeviceList"
-    req = '{"MobileInternalIndex":"' + mii + '","CommandType":"'+ cmd +'"}'
-    ws.send(req)
-    result = ws.recv()
-    ws.close()
-#    if not result:
-#        raise Exception("invalid result")
-    rsp = eval(result)
-#    if rsp['CommandType'] != cmd or rsp['MobileInternalIndex'] != mii:
-#        raise Exception("Request and response mismatches")
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    """Set up the Securifi platform."""
+    # Assign configuration variables.
+    # The configuration check takes care they are present.
+    host = config[CONF_HOST]
+    username = config[CONF_USERNAME]
+    password = config.get(CONF_PASSWORD)
     
-    # Parse through rsp and grab only type 1 and 50 switches
-    # get Data:FriendlyDeviceType, Data:Name, DeviceValues:1:Value
-    devices = {} 
-    for dev in rsp['Devices']:
-        dev_type = rsp['Devices'][dev]['Data']['Type']
-        if dev_type == "1" or dev_type == "50":
-            #print(rsp['Devices'][dev]['Data']['FriendlyDeviceType'])
-            name = rsp['Devices'][dev]['Data']['Name']
-            state = True if rsp['Devices'][dev]['DeviceValues']['1']['Value'].lower() == "true" else False
-            devices[dev] = {"name": name, "state": state}
-            #entities.append(SecurifiSwitch(coordinator, dev, name, state))
+    almond = securifi_almond(host, password, user=username)
 
-    return devices
+    async def async_update_data():
+        """Fetch data from API endpoint.
 
+        This is the place to pre-process the data to lookup tables
+        so entities can quickly look up their data.
+        """
+        try:
+            # Note: asyncio.TimeoutError and aiohttp.ClientError are already
+            # handled by the data update coordinator.
+            async with async_timeout.timeout(10):
+               return almond.refresh_switches()
+        except ApiError as err:
+            raise UpdateFailed(f"Error communicating with API: {err}")
 
-#def setup_platform(hass, config, add_entities, discovery_info=None):
-#async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    coordinator = DataUpdateCoordinator(
+        hass,
+        _LOGGER,
+        # Name of the data. For logging purposes.
+        name="SecurifiSwitch",
+        update_method=async_update_data,
+        # Polling interval. Will only be polled if there are subscribers.
+        update_interval=SCAN_INTERVAL,
+    )
+
+    # Add devices
+    async_add_entities(SecurifiSwitch(coordinator, switch) for switch in almond.get_switches())
+
 async def async_setup_entry(hass, config_entry, async_add_entities):
     """Set up the Securifi platform."""
     # Assign configuration variables.
     # The configuration check takes care they are present.
-    #host = config[CONF_HOST]
-    #username = config[CONF_USERNAME]
-    #password = config.get(CONF_PASSWORD)
     
     host = config_entry.data[CONF_HOST]
     username = config_entry.data[CONF_USERNAME]
@@ -90,8 +86,7 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
             # Note: asyncio.TimeoutError and aiohttp.ClientError are already
             # handled by the data update coordinator.
             async with async_timeout.timeout(10):
-               #return await fetch_switch_data()
-               return almond.get_switches()
+               return almond.refresh_switches()
         except ApiError as err:
             raise UpdateFailed(f"Error communicating with API: {err}")
 
@@ -105,108 +100,58 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
         update_interval=SCAN_INTERVAL,
     )
 
-    # Fetch initial data so we have data when entities subscribe
-    await coordinator.async_refresh()
-    
-    entity = []
-    #for dev in coordinator.data:
-    #   entity.append(SecurifiSwitch(coordinator, str(dev), 
-    #                                             coordinator.data[dev]["name"], 
-    #                                             coordinator.data[dev]["state"]))
-    for dev in coordinator.data:
-       entity.append(SecurifiSwitch(coordinator, str(dev), 
-                                                 coordinator.data[dev]["name"], 
-                                                 coordinator.data[dev]["state"]))
-
     # Add devices
-    #add_entities(entity)
-    async_add_entities(entity)
+    async_add_entities(SecurifiSwitch(coordinator, switch) for switch in almond.get_switches())
 
 
 class SecurifiSwitch(CoordinatorEntity, SwitchEntity):
     """Representation of a Securifi switch (SecurifiSmartSwitch/BinarySwitch)."""
     """ https://wiki.securifi.com/index.php/Devicelist_Documentation """
 
-    def __init__(self, coordinator, devid, name, state):
+    def __init__(self, coordinator, switch):
         """Initialize a Securifi switch."""
         super().__init__(coordinator)
-        self._devid = devid
-        self._name = name
-        self._state = state
+        self._switch = switch
+        self._devid = switch.get_devid()
+        self._name = switch.get_name()
+        #self._state = state
+
 
     @property
     def should_poll(self):
         """Return the polling state."""
         return False
 
+
     @property
     def name(self):
         """Return the display name of this switch."""
         return self._name
 
+
     @property
     def is_on(self):
         """Return true if light is on."""
         #return self._state
-        return self.coordinator.data[self._devid]["state"]
+        return self._switch.get_state()
+        #return self.coordinator.data[self._devid]["state"]
+
 
     @property
     def unique_id(self):
         """Return a unique ID."""
         return f"securifi_almond_switch_{self._devid}"
 
+
     async def async_turn_on(self, **kwargs):
-        """Instruct the switch to turn on.
-        """
-        ws = create_connection("ws://192.168.1.101:7681/admin/third62")
-        result = ws.recv()
-        mii = "1234"
-        cmd = "UpdateDeviceIndex"
-        req = '{"MobileInternalIndex":"' + mii + '","CommandType":"'+ cmd +'", "ID":"' + self._devid + '", "Index":"1", "Value":"true"}'
-        ws.send(req)
-        result = ws.recv()
-        # '{"MobileInternalIndex":"321321","CommandType":"UpdateDeviceIndex","Success":"true"}'
-        ws.close()
-        if not result:
-            raise Exception("invalid result from server")
-        rsp = eval(result)
-        if rsp['CommandType'] != cmd or rsp['MobileInternalIndex'] != mii:
-            raise Exception("Request and response mismatches")
-
-        if rsp['Success'].lower() == "true":
-           self._state = True
-        if rsp['Success'].lower() != "true":
-            _LOGGER.error("Unable to turn on switch")
-
-        #self._light.turn_on()
+        self._switch.turn_on()
         await self.coordinator.async_request_refresh()
 
 
     async def async_turn_off(self, **kwargs):
-        """Instruct the light to turn off."""
-        ws = create_connection("ws://192.168.1.101:7681/admin/third62")
-        result = ws.recv()
-        mii = "1234"
-        cmd = "UpdateDeviceIndex"
-        req = '{"MobileInternalIndex":"' + mii + '","CommandType":"'+ cmd +'", "ID":"' + self._devid + '", "Index":"1", "Value":"false"}'
-        ws.send(req)
-        result = ws.recv()
-        # '{"MobileInternalIndex":"321321","CommandType":"UpdateDeviceIndex","Success":"true"}'
-        ws.close()
-        if not result:
-            raise Exception("invalid result from server")
-        rsp = eval(result)
-        if rsp['CommandType'] != cmd or rsp['MobileInternalIndex'] != mii:
-            raise Exception("Request and response mismatches")
-
-        if rsp['Success'].lower() == "true":
-            self._state = False
-
-        if rsp['Success'].lower() != "true":
-            _LOGGER.error("Unable to turn off switch")
-
-        #self._light.turn_off()
+        self._switch.turn_off()
         await self.coordinator.async_request_refresh()
+
 
 ##    async def async_update(self):
 ##        """Fetch new state data for this light.
